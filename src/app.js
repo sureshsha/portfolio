bb10f36a6ce8feb39ba0a12634363ed14001a85f
unknown <sureshkumar.ece15@gmail.com>
2020-02-02 04:59:55 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '../public' )
+
+app.use(express.static(publicDirectoryPath))
+
+app.set('view engine', 'hbs');
+ 
+app.get('/', function (req, res) {
+  res.render('index')
+})
+ 
+app.listen(3000, ()=> {
+    console.log("server running")
+});
